c2006fa259c40e5c90caefac787ba1aca1e71bc9
giobanny23 <edgar_2_62@hotmail.com>
2021-10-09 01:33:58 -0500

    Agilizando la fluides de Get

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -9,13 +9,15 @@ const usuariosGet =async(req=request, res = response) => {
 
 
     //const {q, nombre ='No name', apikey,page = 1, limit} = req.query;
-    const {limite = 2, desde= 0} = req.query;
+    const {limite = 5, desde= 0} = req.query;
+    const query = {estado: true};
 
-    const usuarios = await Usuario.find()
-        .skip(Number(desde))
-        .limit(Number(limite));
-
-    const total = await Usuario.countDocuments();      
+    const [total, usuarios] = await Promise.all([
+        Usuario.countDocuments(query),
+        Usuario.find(query)
+            .skip(Number(desde))
+            .limit(Number(limite))
+    ]);      
 
     res.json({
         total,
